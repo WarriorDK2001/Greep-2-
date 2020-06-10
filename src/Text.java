52b7b5f6e845f1101aad647d498015e908dda2c9
Ivan <gorden2001@mail.ru>
2020-06-10 21:27:19 +0300

    Задание начато

--- src/Text.java
+++ src/Text.java
@@ -5,32 +5,23 @@ public class Text {
     public static void main(String[] args) throws IOException {
         Scanner regex = new Scanner(System.in);
         String word = regex.next();
-        int counter = 0;
         StringBuilder result = new StringBuilder();
         File output = new File("textTwo.txt");
         File input = new File("textOne.txt");
-        Scanner scanner = new Scanner("textTwo.txt");
-        int lines = 0;
-        while (scanner.hasNextLine()) {
-            scanner.nextLine();
-            lines++;
-        }
-        scanner.close();
-        System.out.println(lines);
+        Scanner scanner = new Scanner(input);
         try {
             FileReader reader = new FileReader(input);
             FileWriter writer = new FileWriter(output);
             BufferedReader bufReader = new BufferedReader(reader);
             BufferedWriter bufWriter = new BufferedWriter(writer);
             String line = bufReader.readLine();
-            lines--;
-            while (lines != 0) {
+            while (scanner.hasNextLine()) {
                 if (line.contains(word)) {
                     result.append(line).append("\n");
                     System.out.println(line);
                     line = bufReader.readLine();
+                    scanner.nextLine();
                 }
-                lines--;
             }
             bufWriter.write(result.toString());
         } catch (IOException e) {
