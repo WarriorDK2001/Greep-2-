0fa1ba2aa6246735be5fcfa80e3d2310112a8e04
Ivan <gorden2001@mail.ru>
2020-06-10 23:44:23 +0300

    Задание частично сделано

--- src/Text.java
+++ src/Text.java
@@ -5,6 +5,7 @@ public class Text {
     public static void main(String[] args) throws IOException {
         Scanner r = new Scanner(System.in);
         String word = r.next();
+        String type = r.next();
         StringBuilder result = new StringBuilder();
         File output = new File("textTwo.txt");
         File input = new File("textOne.txt");
@@ -13,15 +14,38 @@ public class Text {
             FileWriter writer = new FileWriter(output);
             BufferedWriter bufWriter = new BufferedWriter(writer);
             String line = scanner.nextLine();
-            while (scanner.hasNextLine()) {
-                if (line.contains(word)) {
-                    result.append(line).append("\n");
-                    System.out.println(line);
+            if (type.equals("r")) {
+                while (scanner.hasNextLine()) {
+                    if (line.contains(word)) {
+                        result.append(line).append("\n");
+                        System.out.println(line);
+                    }
+                    line = scanner.nextLine();
                 }
-                line = scanner.nextLine();
+            }
+            if (type.equals("i")) {
+                word = word.toLowerCase();
+                line = line.toLowerCase();
+                while (scanner.hasNextLine()) {
+                    if (line.contains(word)) {
+                        result.append(line).append("\n");
+                        System.out.println(line);
+                    }
+                    line = scanner.nextLine();
+                    line = line.toLowerCase();
+                }
+            }
+            if (type.equals("v")) {
+                while (scanner.hasNextLine()) {
+                    if (!line.contains(word)) {
+                        result.append(line).append("\n");
+                        System.out.println(line);
+                    }
+                    line = scanner.nextLine();
+                }
             }
             bufWriter.write(result.toString());
-
+            bufWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
