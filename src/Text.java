e6531061fd121644b03c305eb24dff770ebc13c1
Ivan <gorden2001@mail.ru>
2020-06-10 21:16:43 +0300

    Задание начато

--- src/Text.java
+++ src/Text.java
@@ -9,24 +9,32 @@ public class Text {
         StringBuilder result = new StringBuilder();
         File output = new File("textTwo.txt");
         File input = new File("textOne.txt");
+        Scanner scanner = new Scanner("textTwo.txt");
+        int lines = 0;
+        while (scanner.hasNextLine()) {
+            scanner.nextLine();
+            lines++;
+        }
+        scanner.close();
+        System.out.println(lines);
         try {
             FileReader reader = new FileReader(input);
             FileWriter writer = new FileWriter(output);
             BufferedReader bufReader = new BufferedReader(reader);
             BufferedWriter bufWriter = new BufferedWriter(writer);
             String line = bufReader.readLine();
-            while (counter != 5) {
+            lines--;
+            while (lines != 0) {
                 if (line.contains(word)) {
                     result.append(line).append("\n");
                     System.out.println(line);
                     line = bufReader.readLine();
                 }
-                counter++;
+                lines--;
             }
             bufWriter.write(result.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
