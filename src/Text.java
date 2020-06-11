a29b1896668273218bad9a1a77d8fc8fda5e8cc3
Ivan <gorden2001@mail.ru>
2020-06-11 12:55:41 +0300

    Задание частично сделано

--- src/Text.java
+++ src/Text.java
@@ -3,21 +3,16 @@ import java.util.Scanner;
 
 public class Text {
     public static void main(String[] args) throws IOException {
-        Scanner r = new Scanner(System.in);
-        String word = r.next();
-        String type = r.next();
-        StringBuilder result = new StringBuilder();
-        File output = new File("textTwo.txt");
+       // Scanner r = new Scanner(System.in);
+        String word = "Hello";
+        String type = "r";
         File input = new File("textOne.txt");
         try {
-            Scanner scanner = new Scanner(input);
-            FileWriter writer = new FileWriter(output);
-            BufferedWriter bufWriter = new BufferedWriter(writer);
+            Scanner scanner = new Scanner(input);;
             String line = scanner.nextLine();
             if (type.equals("r")) {
                 while (scanner.hasNextLine()) {
                     if (line.contains(word)) {
-                        result.append(line).append("\n");
                         System.out.println(line);
                     }
                     line = scanner.nextLine();
@@ -28,7 +23,6 @@ public class Text {
                 line = line.toLowerCase();
                 while (scanner.hasNextLine()) {
                     if (line.contains(word)) {
-                        result.append(line).append("\n");
                         System.out.println(line);
                     }
                     line = scanner.nextLine();
@@ -38,14 +32,11 @@ public class Text {
             if (type.equals("v")) {
                 while (scanner.hasNextLine()) {
                     if (!line.contains(word)) {
-                        result.append(line).append("\n");
                         System.out.println(line);
                     }
                     line = scanner.nextLine();
                 }
             }
-            bufWriter.write(result.toString());
-            bufWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
